cee6d736892f223159b29a5d51f320b99846b1d1
zhangjiabin <zhangjiabin@zhongan.com>
2019-06-21 16:37:04 +0800

    迭代器生成器区别示例

--- 迭代器_生成器/code_view1.py
+++ 迭代器_生成器/code_view1.py
@@ -1,9 +1,38 @@
+import os
+import psutil
+
+
 def is_iterable(param):
     try:
         iter(param)
         return True
     except TypeError:
         return False
+
+
+def show_memory_info(hint):
+    pid = os.getpid()
+    p = psutil.Process(pid)
+
+    info = p.memory_full_info()
+    memory = info.uss / 1024.0 / 1024
+    print("{} memory used: {} MB".format(hint, memory))
+
+
+def test_iterator():
+    show_memory_info("initing iterator")
+    list_1 = [i for i in range(100000000)]
+    show_memory_info("after iterator initiated")
+    print(sum(list_1))
+    show_memory_info("after sum called")
+
+
+def test_generator():
+    show_memory_info("initing generator")
+    list_1 = (i for i in range(100000000))
+    show_memory_info("after generator initiated")
+    print(sum(list_1))
+    show_memory_info("after sum called")
 
 
 if __name__ == "__main__":
@@ -17,3 +46,6 @@ if __name__ == "__main__":
     ]
     for param in params:
         print("{} is iterable? {}".format(param, is_iterable(param)))
+
+    test_generator()
+    test_iterator()
